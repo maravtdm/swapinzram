a4287e257d976a968d3b3281dedb994ea2b211b9
marav <11086888+maravtdm@users.noreply.github.com>
2021-04-24 04:05:39 +0200

    Update doinst.sh

--- doinst.sh
+++ doinst.sh
@@ -1,6 +1,6 @@
 config() {
   NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  OLD="$(dirname $NEW)/$(basename $NEW.new)"
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
